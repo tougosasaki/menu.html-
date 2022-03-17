29260ed367ff19a4ccb3211e881391ae70ddaf49
First-tougo Family-sasaki <tougo.sasaki.sys21@morijyobi.ac.jp>
2022-03-17 12:11:31 +0900

    README.mdの編集及びBMI.javaの編集

--- BMI.java
+++ BMI.java
@@ -1,6 +1,19 @@
+
+import java.util.Scanner;
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main (String args[]) {
+
+
+		Scanner sc = new Scanner(System.in);
+		int sin = sc.nextInt();
+		int tai = sc.nextInt();
+
+		double BMI = tai / sin /sin;
+		System.out.println("your BMI is" + BMI);
+		System.out.println(BMI);
+
 	}
+
 }
+
 
